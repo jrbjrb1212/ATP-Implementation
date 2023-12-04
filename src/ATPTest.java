43bc237f864ce8a72ef742b7831cf7a1fd30a005
Mason <904msh@gmail.com>
2023-12-04 14:24:11 -0500

    s

---
+++ src/ATPTest.java
@@ -0,0 +1,66 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class ATPTest {
+
+    private ATP atp;
+
+    @Before
+    public void setUp() {
+        atp = new ATP();
+        // Initialize mocks and other necessary objects
+    }
+
+    @After
+    public void tearDown() {
+        // Clean up resources
+    }
+
+    @Test
+    public void testReserveRide_Success() {
+        String result = atp.reserveRide("vehicleID", "startTime", "endTime");
+        assertEquals("Ride Creation Successful", result);
+    }
+
+    @Test
+    public void testReserveRide_Failure() {
+        String result = atp.reserveRide("vehicleID", "startTime", "endTime");
+        assertEquals("Ride Creation Unsuccessful", result);
+    }
+
+    @Test
+    public void testChangeRide() {
+        // Create a mock or a sample BookedRideData
+        // BookedRideData rideToChange = new BookedRideData(...);
+
+        // Assuming the change request is valid
+        String result = atp.changeRide(rideToChange, "vehicleID", "newStartTime", "newEndTime");
+        assertEquals("Expected success or failure message", result);
+    }
+
+    @Test
+    public void testReturnAllAvailableRides() {
+        // Create a mock or a sample UserReqClass
+        // UserReqClass newUserReq = new UserReqClass(...);
+
+        ArrayList<ATPVehicleData> result = atp.returnAllAvailableRides(newUserReq, "startTime", "endTime");
+        assertNotNull(result);
+        // Additional assertions based on expected behavior
+    }
+
+    @Test
+    public void testDeleteRide() {
+        // Create a mock or a sample BookedRideData
+        // BookedRideData toDelete = new BookedRideData(...);
+
+        String result = atp.deleteRide(toDelete);
+        assertEquals("Expected success or failure message", result);
+    }
+
+    // Additional test methods for any other public methods in the ATP class
+
+    // Optional: Test cases for private methods if necessary, using reflection
+}
